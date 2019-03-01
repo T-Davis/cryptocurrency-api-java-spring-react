4b505092673edceb1cfe2027329bcf62b9ac7c62
Trevor Davis <tzendou@gmail.com>
2019-02-28 16:34:54 -0800

    Add PurchaseHandler listPurchases

---
+++ src/main/java/com/trevor/reactivespring/controller/PurchaseHandler.java
@@ -0,0 +1,18 @@
+package com.trevor.reactivespring.controller;
+
+import com.trevor.reactivespring.Model.Purchase;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+public class PurchaseHandler {
+
+    public Mono<ServerResponse> listPurchases(ServerRequest serverRequest) {
+        final Mono<Purchase> purchase = Mono.fromSupplier(
+                () -> new Purchase("ffe", "123", LocalDateTime.now()));
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(purchase, Purchase.class);
+    }
+}
