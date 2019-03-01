4e08bcec2676510aac2506aab8617f61f4c8de60
Trevor Davis <tzendou@gmail.com>
2019-02-28 17:27:20 -0800

    Configure datasource

---
+++ src/main/java/com/trevor/reactivespring/config/DataConfig.java
@@ -0,0 +1,23 @@
+package com.trevor.reactivespring.config;
+
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@EnableReactiveMongoRepositories
+//@Configuration
+public class DataConfig extends AbstractReactiveMongoConfiguration {
+
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create("mongodb://localhost:27017");
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "reactivespring";
+
+    }
+}
--- src/main/java/com/trevor/reactivespring/config/WebConfig.java
+++ src/main/java/com/trevor/reactivespring/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -12,7 +13,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 @EnableWebFlux
-public class WebConfig {
+public class WebConfig implements WebFluxConfigurer {
 
     @Bean
     public PurchaseHandler purchaseHandler() {
