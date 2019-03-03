ed6d277c934a656fe70fcfb1037ac5819a296867
Trevor Davis <tzendou@gmail.com>
2019-03-02 23:07:22 -0800

    Implement createPurchase() coinbaseService

--- src/main/java/com/trevor/reactivespring/controller/PurchaseController.java
+++ src/main/java/com/trevor/reactivespring/controller/PurchaseController.java
@@ -1,22 +1,27 @@
 package com.trevor.reactivespring.controller;
 
 import com.trevor.reactivespring.Model.Purchase;
+import com.trevor.reactivespring.service.CoinbaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDateTime;
-
 @RestController
 @RequestMapping("/coin/purchase/v1")
 public class PurchaseController {
 
+    private CoinbaseService coinbaseService;
+
+    @Autowired
+    public PurchaseController(CoinbaseService coinbaseService) {
+        this.coinbaseService = coinbaseService;
+    }
+
     @PostMapping(value = "/{cryptoName}")
     public Mono<Purchase> createPurchase(@PathVariable String cryptoName) {
-        return Mono.fromSupplier(
-                () -> new Purchase("cryptoName", "price", LocalDateTime.now())
-        );
+        return coinbaseService.createPurchase(cryptoName);
     }
 }
